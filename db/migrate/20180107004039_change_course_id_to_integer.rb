94faa26d41a53c8938c23b93ccb01e14f7074d98
White W.Witcher <444896219@qq.com>
2018-01-07 09:22:45 +0800

    cast integer for course_id

--- db/migrate/20180107004039_change_course_id_to_integer.rb
+++ db/migrate/20180107004039_change_course_id_to_integer.rb
@@ -1,5 +1,5 @@
 class ChangeCourseIdToInteger < ActiveRecord::Migration[5.1]
   def change
-    change_column :assignments, :course_id, :integer
+    change_column :assignments, :course_id, 'integer USING CAST(course_id AS integer)'
   end
 end
