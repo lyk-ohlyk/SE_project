2c98334c013c4d540c83b43a0224766253125fea
wizlyk <444896219@qq.com>
2018-01-11 13:24:11 +0800

    nothing

--- db/migrate/20180107004039_change_course_id_to_integer.rb
+++ db/migrate/20180107004039_change_course_id_to_integer.rb
@@ -1,7 +1,7 @@
 class ChangeCourseIdToInteger < ActiveRecord::Migration[5.1]
   def change
     # this line is only for PGSQL
-    change_column :assignments, :course_id, 'integer USING CAST(course_id AS integer)'
+    # change_column :assignments, :course_id, 'integer USING CAST(course_id AS integer)'
 
     # this line is for SQLite3
     # change_column :assignments, :course_id, :integer
